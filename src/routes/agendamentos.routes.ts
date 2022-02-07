3df814891488e27eabdf810a76368b1d2441fea6
Luiz Fernando Thomaz <lft.thomaz@gmail.com>
2022-02-06 22:18:22 -0300

    validando se já existe um novo agendamento
    Trabalhando com o pacote "date-fns," no primeiro usando horas para sabe se aquele agendamento já foi feito.

--- src/routes/agendamentos.routes.ts
+++ src/routes/agendamentos.routes.ts
@@ -1,19 +1,39 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const agendamentosRouter = Router();
 
-const agendamentos = [];
+interface Agendamento {
+  id: string;
+  fornecedor: string;
+  data: Date;
+}
+
+const agendamentos: Agendamento[] = [];
 
 agendamentosRouter.post('/', (request, response) => {
-
   const { fornecedor, data } = request.body;
+
+  const novaData = startOfHour(parseISO(data));
+  const encontraraAgendamentoNaMesmaData =
+    agendamentos.find(
+      agendamento => isEqual(novaData, agendamento.data)
+    )
+
+  if (encontraraAgendamentoNaMesmaData) {
+    return response
+      .status(400)
+      .json({
+        message: 'Este agendamento já está reservado'
+      });
+  }
 
   const agendamento = {
     id: uuid(),
     fornecedor,
-    data
-  }
+    data: novaData,
+  };
 
   agendamentos.push(agendamento);
 
